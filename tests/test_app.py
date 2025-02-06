8e2ad96ac24549bb125c3dbfbc3a745385526619
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-02-06 16:17:07 -0300

    new dependencie

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,17 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fastapiproj.app import app
 
-import pytest
 
-@pytest.fixture()
+@pytest.fixture
 def client():
     return TestClient(app)
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
 
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
@@ -23,9 +22,7 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     }
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/add',
         json={
