de4f7c39324cb6b11c3acf9999b394dcf5fb3b26
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-02-06 14:14:44 -0300

    Curso - FastAPI - Day 2

--- fastapiproj/app.py
+++ fastapiproj/app.py
@@ -1,8 +1,10 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from fastapiproj.schema.schema import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def new_route():
-    return {'message': 'Feliz Natal!', 'anothermessage': 'Brutalist!'}
+    return {'message': 'Feliz Natal!', 'value': 11, 'author': 'João'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,12 +6,12 @@ from fastapiproj.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) #Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') #Act
+    response = client.get('/')  # Act
 
-    assert response.status_code == HTTPStatus.OK #Assert 
-    assert response.json() == { #Assert 
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {  # Assert
         'message': 'Feliz Natal!',
         'anothermessage': 'Brutalist!',
     }
