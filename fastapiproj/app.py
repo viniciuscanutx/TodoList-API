0559419c95068d525f9f7f03185a300f489f18af
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-03-06 09:24:23 -0300

    Rotas de administrador.

--- fastapiproj/app.py
+++ fastapiproj/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from fastapiproj.routers import auth, users, welcome
+from fastapiproj.routers import adm_routes, auth, users, welcome
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(welcome.router)
+app.include_router(adm_routes.router)
