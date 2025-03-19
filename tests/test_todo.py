26369ec1c6da249625d5c3809686e0f5fc019a54
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-03-19 13:18:56 -0300

    teste para criação de todos na rota '/create'

---
+++ tests/test_todo.py
@@ -0,0 +1,18 @@
+def test_create_todo(client, token):
+    response = client.post(
+        '/todos/',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'title': 'Test todo',
+            'description': 'Test todo description',
+            'category': 'Test',
+            'state': 'draft',
+        },
+    )
+    assert response.json() == {
+        'id': 1,
+        'title': 'Test todo',
+        'description': 'Test todo description',
+        'category': 'Test',
+        'state': 'draft',
+    }
