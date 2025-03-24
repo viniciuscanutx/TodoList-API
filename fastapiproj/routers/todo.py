124c1362788697f4f7b4ac8b181ca07290954dbf
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-03-24 15:18:27 -0300

    Rotas para listagem de 'todos' de acordo com a conta logada.

--- fastapiproj/routers/todo.py
+++ fastapiproj/routers/todo.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 
 from fastapiproj.config.database import get_session
@@ -32,3 +33,29 @@ def create_todo(
     session.refresh(db_todo)
 
     return db_todo
+
+
+@router.get('/get')
+def list_todos(  # noqa
+    session: Session,  # type: ignore
+    user: CurrentUser,
+    title: str | None = None,
+    category: str | None = None,
+    state: str | None = None,
+    offset: int | None = None,
+    limit: int | None = None,
+):
+    query = select(Todo).where(Todo.user_id == user.id)
+
+    if title:
+        query = query.where(Todo.title.contains(title))
+
+    if category:
+        query = query.where(func.lower(Todo.category) == func.lower(category))
+
+    if state:
+        query = query.where(Todo.state == state)
+
+    todos = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {'todos': todos}
