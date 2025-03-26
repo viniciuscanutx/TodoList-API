134a94eb938c9e184c055d42c45d3672b91870b7
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-03-26 12:30:42 -0300

    Testes unitários para a rota de patch.

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -141,3 +141,28 @@ def test_delete_todo_not_found(session, client, user, token):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Todo não encontrado!'}
+
+
+def test_patch_todo_error(client, token):
+    response = client.patch(
+        '/todos/patch/10',
+        json={},
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Todo não encontrado!'}
+
+
+def test_patch_todo(session, client, user, token):
+    todo = TodoFactory(user_id=user.id)
+
+    session.add(todo)
+    session.commit()
+
+    response = client.patch(
+        f'/todos/patch/{todo.id}',
+        json={'title': 'teste!'},
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()['title'] == 'teste!'
