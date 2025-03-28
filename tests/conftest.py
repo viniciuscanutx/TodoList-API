2115edc2fcd49d8c25ea7409248359e41e7e75a5
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-03-28 11:39:13 -0300

    Usando o testcontainer para os testes e alterando o funcionamento dos testes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,9 @@ import factory
 import factory.fuzzy
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import StaticPool, create_engine, event
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
+from testcontainers.postgres import PostgresContainer
 
 from fastapiproj.app import app
 from fastapiproj.config.database import get_session
@@ -48,17 +49,22 @@ def client(session):
     app.dependency_overrides.clear()
 
 
-@pytest.fixture
-def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+@pytest.fixture(scope='session')  # noqa
+def engine():
+    with PostgresContainer('postgres:14', driver='psycopg') as postgres:
+        _engine = create_engine(postgres.get_connection_url())
+
+        with _engine.begin():
+            yield _engine
+
+
+@pytest.fixture(scope='function')  # noqa
+def session(engine):
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
         yield session
+        session.rollback()
 
     table_registry.metadata.drop_all(engine)
 
